7ec44e81e0207745749f785a8306215ecb32eaaa
phonglamm <lamphong1507@gmail.com>
2025-06-07 21:48:42 +0700

    update

--- src/services/adminServices.jsx
+++ src/services/adminServices.jsx
@@ -32,7 +32,8 @@ const request = async (method, url, data = null, headers = {}, params = {}) => {
 
 const adminService = {
   getAllGym: (params) => request("GET", "v1/gym", null, {}, params),
-  addGym: (data) => request("POST", "v1/gym", data),
+  addGym: (data) =>
+    request("POST", "v1/gym", data, { "Content-Type": "multipart/form-data" }),
   getAllPT: (params) => request("GET", "v1/admin/get-pt", null, {}, params),
 };
 
